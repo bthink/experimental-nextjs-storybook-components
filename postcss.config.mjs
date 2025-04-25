966a02981414994ad0a2ed1e71b6945e027eb309
bthink <bartoszfink@gmail.com>
2025-04-25 15:09:38 +0200

    init

---
+++ postcss.config.mjs
@@ -0,0 +1,10 @@
+// import tailwindcssPostcssPlugin from "@tailwindcss/postcss";
+
+const config = {
+  plugins: [
+    // tailwindcssPostcssPlugin
+    "@tailwindcss/postcss",
+  ],
+};
+
+export default config;
